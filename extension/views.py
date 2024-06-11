f303787c7d0c65d18def7dd5a6b5f55fa5961c48
Nick <nick@trkl.dev>
2024-06-12 07:46:57 +1000

    Add views, urls, etc.

--- extension/views.py
+++ extension/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+class ExtensionView(generic.View):
+    template_name = "extension/main.html"
+    
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
