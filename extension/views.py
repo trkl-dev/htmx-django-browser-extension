d3c36fc408c57cad28e08d7c445d6d1c6bda4395
Nick <nick@trkl.dev>
2024-06-17 16:37:39 +1000

    Update tutorial code

--- extension/views.py
+++ extension/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.shortcuts import render
 from django.views import generic
 
@@ -5,5 +7,7 @@ class ExtensionView(generic.View):
     template_name = "extension/main.html"
     
     def get(self, request):
+        # Sleep for 3 seconds to simulate lengthy backend processing
+        time.sleep(3)
         return render(request, self.template_name, {})
 
